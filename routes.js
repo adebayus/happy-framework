4103a7c7b65b3ecd221c61dbb2082e029c4f5998
adebayus <adebayus17@gmail.com>
2021-04-25 13:01:27 +0700

    [happy][latihan] Method/Verb Request dan Split Routing

--- routes.js
+++ routes.js
@@ -3,14 +3,35 @@ const routes = [
 		method: "GET",
 		path: "/",
 		handler: (request, h) => {
-			return "hello world";
+			return "hello Homepage";
+		},
+	},
+	{
+		method: "*",
+		path: "/",
+		handler: (request, h) => {
+			return "Halaman tidak dapat diakses dengan method tersebut";
 		},
 	},
 	{
 		method: "GET",
 		path: "/about",
 		handler: (request, h) => {
-			return "hello world";
+			return "hello about";
+		},
+	},
+	{
+		method: "*",
+		path: "/about",
+		handler: (request, h) => {
+			return "Halaman tidak dapat diakses dengan method tersebut";
+		},
+	},
+	{
+		method: "*",
+		path: "/{any*}",
+		handler: (request, h) => {
+			return "Halaman tidak dapat diakses dengan method tersebut";
 		},
 	},
 ];
