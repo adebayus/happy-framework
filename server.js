ac5a799610937d6ae991fbb38f7f0c2e1615d236
adebayus <adebayus17@gmail.com>
2021-04-25 12:53:49 +0700

    [happy] Method/Verb Request dan Split Routing

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 const Hapi = require("@hapi/hapi");
-
+const routes = require("./routes");
 const init = async () => {
 	const server = Hapi.server({
 		port: 5000,
 		host: "localhost",
 	});
 
-	server.route({
-		method: "GET",
-		path: "/",
-		handler: (request, h) => {
-			return "hello world";
-		},
-	});
+	server.route(routes);
 
 	await server.start();
 	console.log(`'server berjalan pada ${server.info.uri}'`);
